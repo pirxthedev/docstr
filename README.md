ffe93ef52c32a18fe0ca1865751e0d5c3598137e
Pirx <82919676+pirxthedev@users.noreply.github.com>
2023-02-21 21:44:48 -0500

    Add TipTap collaboration using y-webrtc and local signaling server

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # dostr
 Real-time collaborative document editing over nostr
+
+# Development
+
+## WebRTC Signaling Server
+
+To use our own local WebRTC signaling server, use the one provided with y-webrtc (see https://github.com/yjs/y-webrtc#signaling):
+
+```
+# start signaling server
+set PORT=4444
+node ./node_modules/y-webrtc/bin/server.js
+```
+
--- src/Tiptap.jsx
+++ src/Tiptap.jsx
@@ -1,10 +1,26 @@
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import * as Y from 'yjs'
+import { WebrtcProvider } from 'y-webrtc'
+import Collaboration from '@tiptap/extension-collaboration'
+
+const ydoc = new Y.Doc()
+const provider = new WebrtcProvider(
+  'example-document',
+  ydoc,
+  { signaling: ['ws://localhost:4444'] },
+)
 
 const Tiptap = () => {
   const editor = useEditor({
-    extensions: [StarterKit],
-    content: "<p>Hello World! 🌎️</p>",
+    extensions: [
+      StarterKit.configure({
+        history: false,
+      }),
+      Collaboration.configure({
+        document: ydoc,
+      }),
+    ],
   });
 
   return <EditorContent editor={editor} />;
